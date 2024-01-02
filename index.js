aa2b0ab49e9007971b90b8a67bbb7f015698c69f
irfan <95484470+irfan609@users.noreply.github.com>
2024-01-02 14:57:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ app.post('/calculate', (req, res) => {
   res.send(result);
 });
 
-cron.schedule('56 14 * * *', async () => {
+cron.schedule('58 14 * * *', async () => {
   try {
     console.log('Cron job started for daily update.');
     
