50437db001e37f83c56d6a36281e0b4be95631be
irfan <95484470+irfan609@users.noreply.github.com>
2024-01-09 11:48:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ app.get('/blogs', (req, res) => {
 });
 
 
-cron.schedule('11 48 * * *', async () => {
+cron.schedule('11 49 * * *', async () => {
   try {
     console.log('Cron job started for daily update.');
     
